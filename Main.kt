0023346782e97309f8805b913a04444e9e12883c
90tash <2301020507@cgu-odisha.ac.in>
2025-05-30 23:55:46 +0530

    When Expression
    here, we learned how to use when  expression instead of if/else.
    This expression makes the code more readable

--- Main.kt
+++ Main.kt
@@ -3,16 +3,13 @@ fun main() {
     println("Enter a number:")
     val input = readln()
     val inputAsInteger = input.toIntOrNull()
-    if(inputAsInteger != null){
-        val output = if(inputAsInteger % 2 == 0){
-            "The number is even!"
-        } else if(inputAsInteger < 10){
-            "The number is odd and less than 10"
-        }else{
-            "The number is odd and above than 10 "
-        }
-        println(output)
-    } else {
-        println("Dude, write a valid number!")
+    val output = when(inputAsInteger) {
+        null -> "Dude, write a valid number!"
+        3 -> "The number is three."
+        5 -> "The number is five"
+        in 1..20 -> "The number is between 1 to 20."
+        else -> "Idk, what to print here."
     }
+    println(output)
+
 }
