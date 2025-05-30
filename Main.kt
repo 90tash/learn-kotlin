35eadbd306278004b195c2af766261ef85bea249
90tash <2301020507@cgu-odisha.ac.in>
2025-05-30 23:40:21 +0530

    if conditions implemented
    we learned if else statement and used it in our main.kt file to simplify nullability

--- Main.kt
+++ Main.kt
@@ -2,6 +2,17 @@
 fun main() {
     println("Enter a number:")
     val input = readln()
-    val inputAsInteger = input.toIntOrNull()?.rem(2)?.equals(0) ?: false
-    println("is even? $inputAsInteger")
+    val inputAsInteger = input.toIntOrNull()
+    if(inputAsInteger != null){
+        val output = if(inputAsInteger % 2 == 0){
+            "The number is even!"
+        } else if(inputAsInteger < 10){
+            "The number is odd and less than 10"
+        }else{
+            "The number is odd and above than 10 "
+        }
+        println(output)
+    } else {
+        println("Dude, write a valid number!")
+    }
 }
